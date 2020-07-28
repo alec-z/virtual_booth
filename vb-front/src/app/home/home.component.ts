005eb4abb2ad4cc12fd90b27f3e33d5d3713f161
alec.zheng <aleczheng05@gmail.com>
2020-07-28 15:10:21 +0800

    change detail

--- vb-front/src/app/home/home.component.ts
+++ vb-front/src/app/home/home.component.ts
@@ -14,7 +14,8 @@ export class HomeComponent implements OnInit {
           time: '17：00',
           title: '会议session',
           detail: '【JustinMind培训赋能第二期】主讲人：谢巧华；胡福；时间：2020年7月21日（周二）19:00-21:00；地点：U8-2-A08R；其他地域语音。',
-          meetingType: 'type1'
+          meetingType: 'type1',
+          outerLink: 'http://servicecomb.apache.org'
         },
         {
           time: '17：00',
